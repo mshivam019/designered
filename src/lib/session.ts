349d44fe50017df303c0e62b315c13382becd5ee
Shivam Mishra <mshivam019@gmail.com>
2024-06-10 19:40:20 +0530

    add theme provider, configs and log out button

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,5 +1,4 @@
 import { getServerAuthSession } from "@/server/auth"
-import { getServerSession } from "next-auth/next"
 
 export async function getCurrentUser() {
   const session = await getServerAuthSession();
