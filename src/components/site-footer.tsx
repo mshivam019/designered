1915ce9f831b0bf49b205fd3c4d665bc1fd1b406
Shivam Mishra <mshivam019@gmail.com>
2024-06-14 12:44:40 +0530

    update footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -12,33 +12,16 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <Icons.logo />
                     <p className="text-center text-sm leading-loose md:text-left">
-                        Built by{' '}
+                        Manager
                         <a
                             href={siteConfig.links.twitter}
                             target="_blank"
                             rel="noreferrer"
                             className="font-medium underline underline-offset-4"
                         >
-                            shadcn
+                            by Shivam Mishra
                         </a>
-                        . Hosted on{' '}
-                        <a
-                            href="https://vercel.com"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="font-medium underline underline-offset-4"
-                        >
-                            Vercel
-                        </a>
-                        . Illustrations by{' '}
-                        <a
-                            href="https://popsy.co"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="font-medium underline underline-offset-4"
-                        >
-                            Popsy
-                        </a>
+                        . All rights reserved. © {new Date().getFullYear()}.
                         . The source code is available on{' '}
                         <a
                             href={siteConfig.links.github}
