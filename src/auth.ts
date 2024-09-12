72e8630576daba45becefbc2ac9cf270563ee030
Shivam Mishra <mshivam019@gmail.com>
2024-09-12 22:26:25 +0530

    update name and remove unnecessary code

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,10 @@
 import NextAuth from 'next-auth';
-import NodeMailder from 'next-auth/providers/nodemailer';
+import NodeMailer from 'next-auth/providers/nodemailer';
 import Google from 'next-auth/providers/google';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '@/server/db';
 import {
     accounts,
-    sessions,
     users,
     verificationTokens
 } from '@/server/db/schema';
@@ -17,11 +16,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: DrizzleAdapter(db, {
         usersTable: users,
         accountsTable: accounts,
-        sessionsTable: sessions,
         verificationTokensTable: verificationTokens
     }),
     providers: [
-        NodeMailder({
+        NodeMailer({
             server: process.env.EMAIL_SERVER,
             from: process.env.EMAIL_FROM
         }),
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -68,28 +68,6 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
     user: one(users, { fields: [accounts.userId], references: [users.id] })
 }));
 
-export const sessions = createTable(
-    'session',
-    {
-        sessionToken: varchar('sessionToken', { length: 255 })
-            .notNull()
-            .primaryKey(),
-        userId: varchar('userId', { length: 255 })
-            .notNull()
-            .references(() => users.id),
-        expires: timestamp('expires', {
-            mode: 'date',
-            withTimezone: true
-        }).notNull()
-    },
-    (session) => ({
-        userIdIdx: index('session_userId_idx').on(session.userId)
-    })
-);
-
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-    user: one(users, { fields: [sessions.userId], references: [users.id] })
-}));
 
 export const verificationTokens = createTable(
     'verificationToken',
